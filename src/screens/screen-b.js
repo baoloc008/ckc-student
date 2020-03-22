ef005622cdf3121f923884a843e2e61c323c5686
baoloc008 <baoloc008@gmail.com>
2020-03-22 17:33:18 +0700

    [CKCStudent] intergrate react native webview

--- src/screens/screen-b.js
+++ src/screens/screen-b.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {Component} from 'react';
+import {ActivityIndicator} from 'react-native';
+import {WebView} from 'react-native-webview';
 
-const ScreenB = () => {
-  return <Text>Screen B</Text>;
-};
+class MyWeb extends Component {
+  render() {
+    return (
+      <WebView
+        source={{uri: 'http://caothang.edu.vn/'}}
+        startInLoadingState={true}
+        renderLoading={() => (
+          <ActivityIndicator
+            size="large"
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          />
+        )}
+        style={{
+          position: 'absolute',
+          height: '100%',
+          width: '100%',
+        }}
+      />
+    );
+  }
+}
 
-export default ScreenB;
+export default MyWeb;
